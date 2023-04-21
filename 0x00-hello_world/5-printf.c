b8ef60118265f6a33078f7365bf8b6a40dfeec9e
Fadila218 <fadilaelghzizal@gmail.com>
2023-04-21 14:02:44 +0000

    l

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start
+ *
+ * description - printing with proper grammar,...
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
