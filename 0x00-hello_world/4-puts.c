90735d26975c82c641c8c146e111dca84f7ebf60
Fadila218 <fadilaelghzizal@gmail.com>
2023-04-21 01:49:34 +0000

    c1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@ Include < studio.h >
 /**
  * main - start point
  *
- * description: Write a C program that prints exactly"Programming is like ...",
- * Return:always 0 successful
- */
-Int main(void)}
-	Puts("\"programming is like building a multilingual puzzle");
+ * description: Write a C program that prints with puts
+ * ,
+ * Return:always 0 (successful)
+*/
+Int main(void)
+{	Puts("\"programming is like building a multilingual puzzle");
 	Return(0);
 }
