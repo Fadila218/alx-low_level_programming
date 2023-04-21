b04e0522651291fb49adcb55d6155a10791d6c4a
Fadila218 <fadilaelghzizal@gmail.com>
2023-04-21 01:42:29 +0000

    first main

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+Include < studio.h >
+/**
+ * main - start point
+ *
+ * description: Write a C program that prints exactly"Programming is like ...",
+ * Return:always 0 successful
+ */
+Int main(void)}
+	Puts("\"programming is like building a multilingual puzzle");
+	Return(0);
+}
