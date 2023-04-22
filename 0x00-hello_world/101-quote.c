a5460ed85e8c4e4b36d7d80da2d65228c645c16c
Fadila218 <fadilaelghzizal@gmail.com>
2023-04-22 09:34:59 -0700

    blabla

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - entry
+ *
+ * description - write exactly the sentence
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	write(2, ""and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
